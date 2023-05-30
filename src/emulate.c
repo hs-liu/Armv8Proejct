d619844a9d5090d0f50df749d459733a350c9eb2
jhl21 <jhl21@ic.ac.uk>
2023-05-30 15:31:06 +0100

    Add structs for processor state
    Added after first group meeting with all members present

--- src/emulate.c
+++ src/emulate.c
@@ -1,4 +1,33 @@
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#define MEMORY_CAPACITY (2 * 1024 * 1024)
+
+typedef struct {
+  bool N;
+  bool Z;
+  bool C;
+  bool V;
+} pstate_t;
+
+typedef union {
+  uint32_t W;
+  uint64_t X;
+} reg_t;
+
+typedef struct {
+  reg_t R[31];
+  reg_t ZR;
+  reg_t PC;
+  reg_t SP;
+  pstate_t PSTATE;
+} state_t;
+
+uint8_t main_memory[MEMORY_CAPACITY];
+
+void lsl(state_t *state, int shift_amount) {
+  
+}
 
 int main(int argc, char **argv) {
   return EXIT_SUCCESS;
