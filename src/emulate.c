9dfa9ec984969e8f31a6fe853354240b84d1226a
hl2622, Hongshuo Liu <hongshuo.liu22@imperial.ac.uk>
2023-06-01 13:48:44 +0100

    update the implementation of BRANCH instruction

--- src/emulate.c
+++ src/emulate.c
@@ -14,6 +14,14 @@
 #define REGISTER_BRANCH 0x00000035
 #define CON_BRANCH 0x00000015
 
+#define EQ 0x0
+#define NE 0x1
+#define GE 0xA
+#define LT 0xB
+#define GT 0xC
+#define LE 0xD
+#define AL 0xE
+
 #define OP0_OFFSET 25
 #define OP0_SIZE 4
 
@@ -169,53 +177,40 @@ void emulate_cycle(state_t *cpu_state) {
 void branch_instruction(state_t *cpu_state, uint32_t instruction) {
   uint32_t opcode = instruction >> OP0_OFFSET;
   if (opcode == UNCON_BRANCH) {
-    int64_t extended_offset = (instruction & 0x3FFFFFF) << 38 >> 38;
-    cpu_state->PC.X += extended_offset;
+    int64_t simm26 = (instruction & 0x3FFFFFF) * 4;
+    simm26 = simm26 << 38 >> 38;
+    cpu_state->PC.X += simm26;
   } else if (opcode == REGISTER_BRANCH) {
     int register_index = (((1 << 5) - 1) & (instruction >> 5));
-    cpu_state->PC.X = &(cpu_state->R[register_index]);
+    cpu_state->PC.X = cpu_state->R[register_index].X;
   } else if (opcode == CON_BRANCH) {
-    int64_t extended_offset = ((instruction & 0xFFFFE0) >> 5) << 45 >> 45;
-    switch(instruction & 0xF) {
-      case 0x0:
-          if (cpu_state->PSTATE.Z != 1) {
-            break;
-          }
-          break;
-      case 0x1:
-          if (cpu_state->PSTATE.Z != 0) {
-            break;
-          }
-          break;
-      case 0xA:
-          if (cpu_state->PSTATE.N != 1) {
-            break;
-          }
-          break;
-      case 0xB:
-          if (cpu_state->PSTATE.Z == 1) {
-            break;
-          }
-          break;
-      case 0xC:
-          if (cpu_state->PSTATE.Z != 0 || 
-              cpu_state->PSTATE.N != cpu_state->PSTATE.V) {
-            break;
-          }
-          break;
-      case 0xD:
-          if (cpu_state->PSTATE.Z == 0 && 
-              cpu_state->PSTATE.N == cpu_state->PSTATE.V) {
-            break;
-          }
-          break;
-      case 0xE:
-      default: 
-          cpu_state->PC.X += extended_offset;
-    }
+    int64_t simm19 = ((instruction & 0xFFFFE0) >> 5) * 4;
+    simm19 = simm19 << 45 >> 45;
+    uint8_t cond = instruction & 0xF;
+    conditional_branch(cpu_state, simm19, cond);
   }
 }
 
+void conditional_branch(state_t *cpu_state, int64_t offset, uint8_t cond) {
+  bool jump;
+  switch(cond) {
+    case EQ:
+      jump = cpu_state->PSTATE.Z == 1;
+    case NE:
+      jump = cpu_state->PSTATE.Z == 0;
+    case GE:
+      jump = cpu_state->PSTATE.N == 1;
+    case LT:
+      jump = cpu_state->PSTATE.N != 1;
+    case GT:
+      jump = cpu_state->PSTATE.Z == 0 && cpu_state->PSTATE.N == cpu_state->PSTATE.V;
+    case LE:
+      jump = !(cpu_state->PSTATE.Z == 0 && cpu_state->PSTATE.N == cpu_state->PSTATE.V);
+    case AL:
+    default:
+      cpu_state->PC.X += offset;
+  }
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
