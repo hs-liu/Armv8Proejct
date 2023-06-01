867d9297980aa9157aa05aa4a6eecc8938605bf1
Scott Loo <scottlv95@gmail.com>
2023-06-02 00:14:52 +0100

    Added function execute_dpreg_instruction
    added decode and execute function for dpreg, realised immediate is needed for this session

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,8 +1,12 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+
 #include "emulate.h"
+#include "arithmetic.h"
+
 #define MEMORY_CAPACITY (2 * 1024 * 1024)
 
 typedef struct {
@@ -37,109 +41,109 @@ void set_NV_flags_32(state_t *state, uint32_t result) {
   state->PSTATE.Z = (result == 0);
 }
 
-void and_64(state_t *state, int dest, int src1, int src2) {
+void and_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].X = state->R[src1].X & state->R[src2].X;  
 }
 
-void and_32(state_t *state, int dest, int src1, int src2) {
+void and_32(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].W = state->R[src1].W & state->R[src2].W;  
 }
 
-void bic_64(state_t *state, int dest, int src1, int src2) {
+void bic_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].X = state->R[src1].X & ~state->R[src2].X;  
 }
 
-void bic_32(state_t *state, int dest, int src1, int src2) {
+void bic_32(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].W = state->R[src1].W & ~state->R[src2].W;  
 }
 
-void orr_64(state_t *state, int dest, int src1, int src2) {
+void orr_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].X = state->R[src1].X | state->R[src2].X;
 }
 
-void orr_32(state_t *state, int dest, int src1, int src2) {
+void orr_32(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].W = state->R[src1].W | state->R[src2].W;  
 }
 
-void orn_64(state_t *state, int dest, int src1, int src2) {
+void orn_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].X = state->R[src1].X | ~state->R[src2].X;  
 }
 
-void orn_32(state_t *state, int dest, int src1, int src2) {
+void orn_32(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].W = state->R[src1].W | ~state->R[src2].W;
 }
 
-void eon_64(state_t *state, int dest, int src1, int src2) {
+void eon_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].X = state->R[src1].X ^ ~state->R[src2].X;  
 }
 
-void eon_32(state_t *state, int dest, int src1, int src2) {
+void eon_32(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].W = state->R[src1].W ^ ~state->R[src2].W;  
 }
 
-void ands_64(state_t *state, int dest, int src1, int src2) {
+void ands_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].X = state->R[src1].X & state->R[src2].X;  
   set_NV_flags_64(state, state->R[dest].X);
   state->PSTATE.C = 0;
   state->PSTATE.V = 0;
 }
 
-void ands_32(state_t *state, int dest, int src1, int src2) {
+void ands_32(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].W = state->R[src1].W & state->R[src2].W;  
   set_NV_flags_32(state, state->R[dest].W);
   state->PSTATE.C = 0;
   state->PSTATE.V = 0;
 }
 
-void bics_64(state_t *state, int dest, int src1, int src2) {
+void bics_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].X = state->R[src1].X & ~state->R[src2].X;  
   set_NV_flags_64(state, state->R[dest].X);
   state->PSTATE.C = 0;
   state->PSTATE.V = 0;
 }
 
-void bics_32(state_t *state, int dest, int src1, int src2) {
+void bics_32(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   state->R[dest].W = state->R[src1].W & ~state->R[src2].W;  
   set_NV_flags_32(state, state->R[dest].W);
   state->PSTATE.C = 0;
   state->PSTATE.V = 0;
 }
 
-void madd_64(state_t *state, int dest, int src, int rn, int rm) {
+void madd_64(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) {
   state->R[dest].X = state->R[src].X + state->R[rn].X * state->R[rm].X;
 }
 
-void madd_32(state_t *state, int dest, int src, int rn, int rm) {
+void madd_32(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) {
   state->R[dest].W = state->R[src].W + state->R[rn].W * state->R[rm].W;
 }
 
-void msub_64(state_t *state, int dest, int src, int rn, int rm) {
+void msub_64(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) {
   state->R[dest].X = state->R[src].X - state->R[rn].X * state->R[rm].X;
 }
 
-void msub_32(state_t *state, int dest, int src, int rn, int rm) {
+void msub_32(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) {
   state->R[dest].W = state->R[src].W - state->R[rn].W * state->R[rm].W;
 }
 
-void lsl_64(state_t *state, int operand_reg, int shift_amount) {
+void lsl_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     state->R[operand_reg].X <<= shift_amount;
 }
 
-void lsl_32(state_t *state, int operand_reg, int shift_amount) {
+void lsl_32(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     state->R[operand_reg].W <<= shift_amount;
     state->R[operand_reg].X &= 0x00000000FFFFFFFF;
 }
 
-void lsr_64(state_t *state, int operand_reg, int shift_amount) {
+void lsr_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     state->R[operand_reg].X >>=shift_amount;
 }
 
-void lsr_32(state_t *state, int operand_reg, int shift_amount) {
+void lsr_32(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     state->R[operand_reg].W >>= shift_amount;
     state->R[operand_reg].X &= 0x00000000FFFFFFFF;
 }
 
-void asr_64(state_t *state, int operand_reg, int shift_amount) {
+void asr_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     uint64_t sign_bit = state->R[operand_reg].X >> 63;
     // printf("sign-bit = %llx", sign_bit);
     state->R[operand_reg].X >>= shift_amount;
@@ -149,7 +153,7 @@ void asr_64(state_t *state, int operand_reg, int shift_amount) {
     }
 }
 
-void asr_32(state_t *state, int operand_reg, int shift_amount) {
+void asr_32(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     uint32_t sign_bit = state->R[operand_reg].W >> 31;
     state->R[operand_reg].X &= 0x00000000FFFFFFFF;
     state->R[operand_reg].W >>= shift_amount;
@@ -159,7 +163,7 @@ void asr_32(state_t *state, int operand_reg, int shift_amount) {
     }
 }
 
-void ror_64(state_t *state, int operand_reg, int shift_amount) {
+void ror_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     uint64_t mask = ((1 << (shift_amount + 1 )) -1);
     uint64_t lower_bits = state->R[operand_reg].X & mask;
     lower_bits <<= (64-shift_amount);
@@ -167,7 +171,7 @@ void ror_64(state_t *state, int operand_reg, int shift_amount) {
     state->R[operand_reg].X |= lower_bits;
 }
 
-void ror_32(state_t *state, int operand_reg, int shift_amount) {
+void ror_32(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     uint32_t mask = ((1 << (shift_amount + 1))-1);
     uint32_t lower_bits = state->R[operand_reg].X & mask;
     lower_bits <<= (32-shift_amount);
@@ -176,9 +180,121 @@ void ror_32(state_t *state, int operand_reg, int shift_amount) {
     state->R[operand_reg].W |= lower_bits;
 }
 
-// void execute_dpreg_instruction(state_t *state, uint32_t instruction) {
-     
-// }
+
+void execute_dpreg_instruction(state_t *state, uint32_t instruction) {
+  assert(SELECT_BITS(instruction, DPREG_OFFSET, DPREG_SIZE) == 0x5);
+  uint8_t sf = SELECT_BITS(instruction, SF_OFFSET, SF_SIZE);
+  uint8_t opc = SELECT_BITS(instruction, OPC_OFFSET, OPC_SIZE);
+  uint8_t m = SELECT_BITS(instruction, M_OFFSET, M_SIZE);
+  uint8_t opr = SELECT_BITS(instruction, OPR_OFFSET, OPR_SIZE);
+  uint8_t rm = SELECT_BITS(instruction, RM_OFFSET, RM_SIZE);
+  uint8_t operand = SELECT_BITS(instruction, OPERAND_OFFSET, OPERAND_SIZE);
+  uint8_t rn = SELECT_BITS(instruction, RN_OFFSET, RN_SIZE);
+  uint8_t rd = SELECT_BITS(instruction, RD_OFFSET, RD_SIZE);
+
+  // Check if arithmetic
+  if (m == ARITHMETIC_M && CHECK_BITS(opr, ARITHMETIC_MASK, ARITHMETIC_VALUE)) {
+    uint8_t shift = SELECT_BITS(opr, SHIFT_OFFSET, SHIFT_SIZE);
+    if (sf = SF_32) {
+      assert(operand < 32);
+    switch (shift)
+    {
+      case LSL_VALUE:
+        lsl_32(state, rm, operand);
+        break;
+      case LSR_VALUE:
+        lsr_32(state, rm, operand);
+        break;
+      case ASR_VALUE:
+        asr_32(state, rm, operand);
+        break;
+    }
+    //TODO ARITHMETIC 32 (SAME AS IMMEDIATE)
+    }
+    if (sf = SF_64) {
+      assert(operand < 64);
+    switch (shift)
+    {
+      case LSL_VALUE:
+        lsl_64(state, rm, operand);
+        break;
+      case LSR_VALUE:
+        lsr_64(state, rm, operand);
+        break;
+      case ASR_VALUE:
+        asr_64(state, rm, operand);
+        break;
+    }
+    //TODO ARITHMETIC 64 (SAME AS IMMEDIATE)
+    }
+  }
+
+
+
+  // Check if bit-logic
+  if (m == BIT_LOGIC_M && CHECK_BITS(opr, BIT_LOGIC_MASK, BIT_LOGIC_VALUE)) {
+    uint8_t shift = SELECT_BITS(opr, SHIFT_OFFSET, SHIFT_SIZE);
+    uint8_t N = SELECT_BITS(opr, N_OFFSET, N_SIZE);
+    assert (shift == ROR_VALUE);
+    if (sf = SF_32) {
+    assert(operand < 32);
+    ror_32(state, rm, operand);
+    if (N == 0) {
+      switch(opc) {
+        case AND_OPC:
+          and_32(state, rd, rn, rm);
+          break;
+        case ORR_OPC:
+          orr_32(state, rd, rn, rm);
+          break;
+        case EON_OPC:
+          eon_32(state, rd, rn, rm);
+          break;
+        case ANDS_OPC:
+          ands_32(state, rd, rn, rm);
+          break;
+      }
+    }
+    if (N == 1) {
+      switch(opc) {
+        case AND_OPC:
+          bic_32(state, rd, rn, rm);
+          break;
+        case ORR_OPC:
+          orn_32(state, rd, rn, rm);
+          break;
+        case EON_OPC:
+          eor_32(state, rd, rn, rm);
+          break;
+        case ANDS_OPC:
+          bics_32(state, rd, rn, rm);
+          break;
+      }
+    }
+
+
+  }
+
+  // Check if Multiply
+  if (m == MULTIPLY_M && CHECK_BITS(opr, MULTIPLY_MASK, MULTIPLY_VALUE)) {
+      uint8_t x = SELECT_BITS(operand, 5, 1);
+      uint8_t ra = SELECT_BITS(operand, 0, 5);
+      if (sf == SF_32 && x == MADD_X) {
+          madd_32(&state, rd, ra, rn, rm);
+      }
+      if (sf == SF_32 && x == MSUB_X) {
+          msub_32(&state, rd, ra, rn, rm);
+      }
+      if (sf == SF_64 && x == MADD_X) {
+          madd_64(&state, rd, ra, rn, rm);
+      }
+      if (sf == SF_64 && x == MSUB_X){
+          msub_64(&state, rd, ra, rn, rm);
+        }
+  }
+
+  }
+}
 
 
 int main(int argc, char **argv) {
---
+++ src/arithmetic.h
@@ -0,0 +1,59 @@
+#define SF_32 0
+#define SF_64 1
+
+#define DPREG_OFFSET 25
+#define DPREG_SIZE 3
+
+#define SF_OFFSET 31
+#define SF_SIZE 1
+
+#define OPC_OFFSET 29
+#define OPC_SIZE 2
+
+#define M_OFFSET 28
+#define M_SIZE 1
+
+#define OPR_OFFSET 21
+#define OPR_SIZE 4
+
+#define RM_OFFSET 16
+#define RM_SIZE 5
+
+#define OPERAND_OFFSET 10
+#define OPERAND_SIZE 6
+
+#define RN_OFFSET 5
+#define RN_SIZE 5
+
+#define RD_OFFSET 0
+#define RD_SIZE 5
+
+#define ARITHMETIC_M 0
+#define ARITHMETIC_MASK 0x9
+#define ARITHMETIC_VALUE 0x8
+
+#define BIT_LOGIC_M 0
+#define BIT_LOGIC_MASK 0x8
+#define BIT_LOGIC_VALUE 0x8
+
+#define MULTIPLY_M 1
+#define MULTIPLY_MASK 0xF
+#define MULTIPLY_VALUE 0x8
+
+#define SHIFT_OFFSET 1
+#define SHIFT_SIZE 2
+#define N_OFFSET 0
+#define N_SIZE 1
+
+#define LSL_VALUE 0x0
+#define LSR_VALUE 0x1
+#define ASR_VALUE 0x2
+#define ROR_VALUE 0x3
+
+#define AND_OPC 0x0
+#define ORR_OPC 0x1
+#define EON_OPC 0x2
+#define ANDS_OPC 0x3
+
+#define MADD_X 0
+#define MSUB_X 1
