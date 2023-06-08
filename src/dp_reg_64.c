7a0830351aa02425675a47bfeb48e5b70aafcbce
Scott Loo <syl121@aube.doc.ic.ac.uk>
2023-06-08 13:53:51 +0100

    moved declaration of main memory to emulate.c

--- src/dp_reg_64.c
+++ src/dp_reg_64.c
@@ -43,7 +43,6 @@ void ands_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
 void bics_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
   state->R[dest].X = state->R[src1].X & ~op2;  
   set_NV_flags_64(state, state->R[dest].X);
-  printf("state->R[dest].X: %ld\n", state->R[dest].X);
   state->PSTATE.C = 0;
   state->PSTATE.V = 0;
 }
@@ -71,20 +70,12 @@ uint64_t asr_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
 }
 
 uint64_t ror_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
-    // uint64_t mask = ((1l << (shift_amount + 1 )) -1);
-    printf("state->R[operand_reg].X: %lx\n", state->R[operand_reg].X);
-    printf("shift_amount: %d\n", shift_amount);
     uint64_t lower_bits = SELECT_BITS(state->R[operand_reg].X, 0, shift_amount);
-    printf("lower_bits: %llx\n", lower_bits);
-    uint64_t upper_bits = SELECT_BITS(state->R[operand_reg].X, shift_amount, 64-shift_amount);
-    printf("upper_bits: %lx\n", upper_bits);
+    uint64_t upper_bits = SELECT_BITS(state->R[operand_reg].X, shift_amount, 64 - shift_amount);
     lower_bits <<= (64-shift_amount);
     // uint64_t result = state->R[operand_reg].X >> shift_amount;
     upper_bits |= lower_bits;
     return upper_bits;
-
-
-    
 }
 
 void add_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
@@ -111,7 +102,6 @@ void subs_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
   if (dest != ZR_REG) {
     state->R[dest].X = result;
   }
-  printf("result: %ld\n", result);
   set_NV_flags_64(state, result);
   state->PSTATE.C = (state->R[src1].X < state->R[src2].X);
   state->PSTATE.V = ((state->R[src1].X >> 63) != (state->R[src2].X >> 63)) 
@@ -120,7 +110,6 @@ void subs_64(state_t *state, uint8_t dest, uint8_t src1, uint8_t src2) {
 
 
 void execute_dpreg_instruction_64(state_t *state, uint32_t instruction) {
-  printf("in function execute dpreg instruction 64\n");
   assert(SELECT_BITS(instruction, DPREG_OFFSET, DPREG_SIZE) == 0x5);
   uint8_t sf = SELECT_BITS(instruction, REG_SF_OFFSET, REG_SF_SIZE);
   uint8_t opc = SELECT_BITS(instruction, REG_OPC_OFFSET, REG_OPC_SIZE);
@@ -171,9 +160,7 @@ void execute_dpreg_instruction_64(state_t *state, uint32_t instruction) {
   if (m == BIT_LOGIC_M && CHECK_BITS(opr, BIT_LOGIC_MASK, BIT_LOGIC_VALUE)) {
     uint8_t shift = SELECT_BITS(opr, SHIFT_OFFSET, SHIFT_SIZE);
     uint8_t N = SELECT_BITS(opr, N_OFFSET, N_SIZE);
-    // assert (shift == ROR_VALUE);
     assert(operand < 64);
-    // ror_64(state, rm, operand);
     uint64_t op2 = state->R[rm].X;
 
     switch(shift) {
@@ -225,7 +212,6 @@ void execute_dpreg_instruction_64(state_t *state, uint32_t instruction) {
   }
 
   }
-  printf("m = %d, opr = %d\n", m, opr);
   // Check if Multiply
   if (m == MULTIPLY_M && CHECK_BITS(opr, MULTIPLY_MASK, MULTIPLY_VALUE)) {
     printf("Multiply ran\n");
@@ -239,41 +225,4 @@ void execute_dpreg_instruction_64(state_t *state, uint32_t instruction) {
           msub_64(state, rd, ra, rn, rm);
         }
   }
-
 }
-
-
-// int main(int argc, char **argv) {
-
-//     state_t DUM_STATE = {
-//         .R = {
-//             [0].X = 0xFFFF00000000FFFF,
-//             [1].X = 0xFFFF00000000FFFF,
-//             [2].X = 0xFFFF00000000FFFF,
-//             [3].X = 0xFFFF00000000FFFF,
-//             [4].X = 0x0FFFF0000000FFFF,
-//             [5].X = 0xFFFF00000000FFFF,
-//             [6].X = 0x00FF00000000FFFF,
-//             [7].X = 0xFFFF00000000FFF0,
-//         },
-//         .ZR = {0},
-//         .PC = {0},
-//         .SP = {0},
-//         .PSTATE = {0}
-//     };
-
-//     lsl_64(&DUM_STATE, 0, 4);
-//     lsl_32(&DUM_STATE, 1, 4);
-//     lsr_64(&DUM_STATE, 2, 4);
-//     asr_64(&DUM_STATE, 3, 4);
-//     asr_64(&DUM_STATE, 4, 4);
-//     asr_32(&DUM_STATE, 5, 4);
-//     ror_64(&DUM_STATE, 6, 8);
-//     ror_32(&DUM_STATE, 7, 8);
-
-//     for (int i = 0; i<8; i++) {
-//         printf("register %d = %016lX\n", i, DUM_STATE.R[i].X);
-//     }
-
-//   return EXIT_SUCCESS;
-// }
