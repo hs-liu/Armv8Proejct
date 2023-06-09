5e43f89386ba5b2b411739f6382f4c7e5d390931
Jerome Leow <jhl21@ic.ac.uk>
2023-06-09 10:42:46 +0100

    Combined 32/64 bit dpreg into single file

--- src/dp_reg.c
+++ src/dp_reg.c
@@ -8,10 +8,19 @@
 #include "dp_reg.h"
 #include "dp_imm.h"
 
+void and_32(state_t *state, uint8_t dest, uint8_t src1, uint32_t op2) {
+    uint32_t result = get_register_value_32(state, src1) & op2;
+    set_register_value_32(state, dest, result);
+}
 
 void and_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
     uint64_t result = get_register_value_64(state, src1) & op2;
     set_register_value_64(state, dest, result);
+}
+
+void bic_32(state_t *state, uint8_t dest, uint8_t src1, uint32_t op2) {
+    uint32_t result = get_register_value_32(state, src1) & ~op2;
+    set_register_value_32(state, dest, result);
 }
 
 void bic_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
@@ -19,14 +28,28 @@ void bic_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
     set_register_value_64(state, dest, result);
 }
 
+void orr_32(state_t *state, uint8_t dest, uint8_t src1, uint32_t op2) {
+    uint32_t result = get_register_value_32(state, src1) | op2;
+    set_register_value_32(state, dest, result);
+}
+
 void orr_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
     uint64_t result = get_register_value_64(state, src1) | op2;
     set_register_value_64(state, dest, result);
 }
 
+void orn_32(state_t *state, uint8_t dest, uint8_t src1, uint32_t op2) {
+    uint32_t result = get_register_value_32(state, src1) | ~op2;
+    set_register_value_32(state, dest, result);
+}
+
 void orn_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
     uint64_t result = get_register_value_64(state, src1) | ~op2;
     set_register_value_64(state, dest, result);
+}
+void eon_32(state_t *state, uint8_t dest, uint8_t src1, uint32_t op2) {
+    uint32_t result = get_register_value_32(state, src1) ^ ~op2;
+    set_register_value_32(state, dest, result);
 }
 
 void eon_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
@@ -34,15 +57,35 @@ void eon_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
     set_register_value_64(state, dest, result);
 }
 
+void eor_32(state_t *state, uint8_t dest, uint8_t src1, uint32_t op2) {
+    uint32_t result = get_register_value_32(state, src1) ^ op2;
+    set_register_value_32(state, dest, result);
+}
+
 void eor_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
     uint64_t result = get_register_value_64(state, src1) ^ op2;
     set_register_value_64(state, dest, result);
+}
+
+void ands_32(state_t *state, uint8_t dest, uint8_t src1, uint32_t op2) {
+    uint32_t result = get_register_value_32(state, src1) & op2;
+    set_register_value_32(state, dest, result);
+    set_NV_flags_32(state, result);
+    state->PSTATE.C = 0;
+    state->PSTATE.V = 0;
 }
 
 void ands_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
     uint64_t result = get_register_value_64(state, src1) & op2;
     set_register_value_64(state, dest, result);
     set_NV_flags_64(state, result);
+    state->PSTATE.C = 0;
+    state->PSTATE.V = 0;
+}
+void bics_32(state_t *state, uint8_t dest, uint8_t src1, uint32_t op2) {
+    uint32_t result = get_register_value_32(state, src1) & ~op2;
+    set_register_value_32(state, dest, result);
+    set_NV_flags_32(state, result);
     state->PSTATE.C = 0;
     state->PSTATE.V = 0;
 }
@@ -55,10 +98,22 @@ void bics_64(state_t *state, uint8_t dest, uint8_t src1, uint64_t op2) {
     state->PSTATE.V = 0;
 }
 
+void madd_32(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) {
+    uint32_t result = get_register_value_32(state, src);
+    result += get_register_value_32(state, rn) * get_register_value_32(state, rm);
+    set_register_value_32(state, dest, result);
+}
+
 void madd_64(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) {
     uint64_t result = get_register_value_64(state, src);
     result += get_register_value_64(state, rn) * get_register_value_64(state, rm);
     set_register_value_64(state, dest, result);
+}
+
+void msub_32(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) {
+    uint32_t result = get_register_value_32(state, src);
+    result -= get_register_value_32(state, rn) * get_register_value_32(state, rm);
+    set_register_value_32(state, dest, result);
 }
 
 void msub_64(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) {
@@ -67,18 +122,41 @@ void msub_64(state_t *state, uint8_t dest, uint8_t src, uint8_t rn, uint8_t rm) 
     set_register_value_64(state, dest, result);
 }
 
+uint32_t lsl_32(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
+    return get_register_value_32(state, operand_reg) << shift_amount;
+}
+
 uint64_t lsl_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     return get_register_value_64(state, operand_reg) << shift_amount;
+}
+
+uint32_t lsr_32(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
+    return get_register_value_32(state, operand_reg) >> shift_amount;
 }
 
 uint64_t lsr_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     return get_register_value_64(state, operand_reg) >> shift_amount;
 }
 
+uint32_t asr_32(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
+    int32_t result = get_register_value_32(state, operand_reg);
+    result >>= shift_amount;
+    return (uint32_t) result;
+}
+
 uint64_t asr_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     int64_t result = get_register_value_64(state, operand_reg);
     result >>= shift_amount;
     return (uint64_t) result;
+}
+
+uint32_t ror_32(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
+    uint32_t operand = get_register_value_32(state, operand_reg);
+    uint32_t lower_bits = SELECT_BITS(operand, 0, shift_amount);
+    uint32_t upper_bits = SELECT_BITS(operand, shift_amount, 32 - shift_amount);
+    lower_bits <<= (32 - shift_amount);
+    upper_bits |= lower_bits;
+    return upper_bits;
 }
 
 uint64_t ror_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
@@ -88,6 +166,123 @@ uint64_t ror_64(state_t *state, uint8_t operand_reg, uint8_t shift_amount) {
     lower_bits <<= (64 - shift_amount);
     upper_bits |= lower_bits;
     return upper_bits;
+}
+
+void execute_dpreg_instruction_32(state_t *state, uint32_t instruction) {
+    assert(SELECT_BITS(instruction, DPREG_OFFSET, DPREG_SIZE) == 0x5);
+    uint8_t sf = SELECT_BITS(instruction, REG_SF_OFFSET, REG_SF_SIZE);
+    uint8_t opc = SELECT_BITS(instruction, REG_OPC_OFFSET, REG_OPC_SIZE);
+    uint8_t m = SELECT_BITS(instruction, REG_M_OFFSET, REG_M_SIZE);
+    uint8_t opr = SELECT_BITS(instruction, REG_OPR_OFFSET, REG_OPR_SIZE);
+    uint8_t rm = SELECT_BITS(instruction, REG_RM_OFFSET, REG_RM_SIZE);
+    uint8_t operand = SELECT_BITS(instruction, REG_OPERAND_OFFSET, REG_OPERAND_SIZE);
+    uint8_t rn = SELECT_BITS(instruction, REG_RN_OFFSET, REG_RN_SIZE);
+    uint8_t rd = SELECT_BITS(instruction, REG_RD_OFFSET, REG_RD_SIZE);
+
+    // Check if arithmetic
+    if (m == ARITHMETIC_M && CHECK_BITS(opr, ARITHMETIC_MASK, ARITHMETIC_VALUE)) {
+        uint8_t shift = SELECT_BITS(opr, SHIFT_OFFSET, SHIFT_SIZE);
+        assert(sf == SF_32);
+        assert(operand < 32);
+        uint32_t op2;
+        switch (shift) {
+            case LSL_VALUE:
+                op2 = lsl_32(state, rm, operand);
+                break;
+            case LSR_VALUE:
+                op2 = lsr_32(state, rm, operand);
+                break;
+            case ASR_VALUE:
+                op2 = asr_32(state, rm, operand);
+                break;
+        }
+
+        switch (opc) {
+            case ADD_OPC:
+                add_32_imm(state, rd, rn, op2);
+                break;
+            case ADDS_OPC:
+                adds_32_imm(state, rd, rn, op2);
+                break;
+            case SUB_OPC:
+                sub_32_imm(state, rd, rn, op2);
+                break;
+            case SUBS_OPC:
+                subs_32_imm(state, rd, rn, op2);
+                break;
+        }
+    }
+
+    // Check if bit-logic
+    if (m == BIT_LOGIC_M && CHECK_BITS(opr, BIT_LOGIC_MASK, BIT_LOGIC_VALUE)) {
+        uint8_t shift = SELECT_BITS(opr, SHIFT_OFFSET, SHIFT_SIZE);
+        uint8_t N = SELECT_BITS(opr, N_OFFSET, N_SIZE);
+        assert(operand < 32);
+        assert(sf == SF_32);
+        uint32_t op2;
+
+        switch (shift) {
+            case LSL_VALUE:
+                op2 = lsl_32(state, rm, operand);
+                break;
+            case LSR_VALUE:
+                op2 = lsr_32(state, rm, operand);
+                break;
+            case ASR_VALUE:
+                op2 = asr_32(state, rm, operand);
+                break;
+            case ROR_VALUE:
+                op2 = ror_32(state, rm, operand);
+                break;
+        }
+
+        if (N == 0) {
+            switch (opc) {
+                case AND_OPC:
+                    and_32(state, rd, rn, op2);
+                    break;
+                case ORR_OPC:
+                    orr_32(state, rd, rn, op2);
+                    break;
+                case EON_OPC:
+                    eor_32(state, rd, rn, op2);
+                    break;
+                case ANDS_OPC:
+                    ands_32(state, rd, rn, op2);
+                    break;
+            }
+        }
+        if (N == 1) {
+            switch (opc) {
+                case AND_OPC:
+                    bic_32(state, rd, rn, op2);
+                    break;
+                case ORR_OPC:
+                    orn_32(state, rd, rn, op2);
+                    break;
+                case EON_OPC:
+                    eon_32(state, rd, rn, op2);
+                    break;
+                case ANDS_OPC:
+                    bics_32(state, rd, rn, op2);
+                    break;
+            }
+        }
+
+
+    }
+    // Check if Multiply
+    if (m == MULTIPLY_M && CHECK_BITS(opr, MULTIPLY_MASK, MULTIPLY_VALUE)) {
+        uint8_t x = SELECT_BITS(instruction, REG_X_OFFSET, REG_X_SIZE);
+        uint8_t ra = SELECT_BITS(instruction, REG_RA_OFFSET, REG_RA_SIZE);
+        assert(sf == SF_32);
+        if (x == MADD_X) {
+            madd_32(state, rd, ra, rn, rm);
+        }
+        if (x == MSUB_X) {
+            msub_32(state, rd, ra, rn, rm);
+        }
+    }
 }
 
 void execute_dpreg_instruction_64(state_t *state, uint32_t instruction) {
