f828ab2319345b9887b93df99ae2deefb3b74149
Yukie <yz8922@ic.ac.uk>
2023-06-15 13:03:01 +0100

    added structs

--- rpi/led.c
+++ rpi/led.c
@@ -1,5 +1,3 @@
 
-#define SELECT_BITS(value, offset, size) (((value) >> (offset)) & ((1ll << (size)) - 1))
-#define CHECK_BITS(value, mask, target_value) (((value) & (mask)) == (target_value))
-#define SIGN_EXT(value, in_size, out_size) ((value) << ((out_size) - (in_size)) >> ((out_size) - (in_size)))
+
 
---
+++ rpi/led.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define SELECT_BITS(value, offset, size) (((value) >> (offset)) & ((1ll << (size)) - 1))
+#define CHECK_BITS(value, mask, target_value) (((value) & (mask)) == (target_value))
+#define SIGN_EXT(value, in_size, out_size) ((value) << ((out_size) - (in_size)) >> ((out_size) - (in_size)))
+
+#define LED_CHANNEL 8
+
+typedef struct {
+    uint8_t channel: 4; // : defines how many bits channel uses
+    uint32_t data: 28;
+} mailbox_message_t;
+
+typedef struct {
+    uint32_t reserved: 30;
+    uint8_t E: 1;
+    uint8_t F: 1;
+} mail_status_t;
