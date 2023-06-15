4724b45ba3ac200e9d0455b5bdf3b6822c29fef4
Yukie <yz8922@ic.ac.uk>
2023-06-15 12:00:22 +0100

    useful macro functions

--- rpi/led.c
+++ rpi/led.c
@@ -1,1 +1,5 @@
 
+#define SELECT_BITS(value, offset, size) (((value) >> (offset)) & ((1ll << (size)) - 1))
+#define CHECK_BITS(value, mask, target_value) (((value) & (mask)) == (target_value))
+#define SIGN_EXT(value, in_size, out_size) ((value) << ((out_size) - (in_size)) >> ((out_size) - (in_size)))
+
