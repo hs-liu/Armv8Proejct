7cebf9d14977b10adb75f88c6428b0c96aadd6b2
Hongshuo Liu <hl2622@aube.doc.ic.ac.uk>
2023-06-15 14:04:33 +0100

    add on mailbox macros in rpi.h

--- rpi/led.h
+++ rpi/led.h
@@ -6,8 +6,16 @@
 #define LED_CHANNEL 8
 #define BASE_ADDRESS 0x3f000000
 #define READ_OFFSET 0x00
-#define WRITE_REGISTER 0x20
-#define STATUS_REGISTER 0x38
+#define WRITE_OFFSET 0x20
+#define STATUS_OFFSET 0x38
+#define MAILBOX_OFFSET 0xb880
+
+#define MAILBOX_ADDRESS BASE_ADDRESS+MAILBOX_OFFSET
+#define MAILBOX_READ ((mailbox_message_t *)(BASE_ADDRESS+READ_OFFSET))
+#define MAILBOX_WRITE ((mailbox_message_t *)(BASE_ADDRESS+WRITE_OFFSET))
+#define MAILBOX_STATUS ((mailbox_message_t *)(BASE_ADDRESS+STATUS_OFFSET))
+
+
 
 typedef struct {
     uint8_t channel: 4; // : defines how many bits channel uses
@@ -24,3 +32,5 @@ typedef struct {
     mailbox_message_t mails[10];
     uint16_t len;
 } mail_queue;
+
+void mailbox_sent(mail_queue *requests, mailbox_message_t mag);
