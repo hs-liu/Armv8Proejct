d0df0d7bc0245ce2cdcc69afe8ee2d88f52f900c
jhl21 <jhl21@ic.ac.uk>
2023-06-15 15:57:00 +0100

    Implment unit tests
    Coauthored by Scott

--- src/test.c
+++ src/test.c
@@ -2,8 +2,12 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "symbol_table.h"
+#include "assemble.h"
+#include "assemble_dt.h"
+#include "utils.h"
 
 void test_hashmap(void);
 
@@ -39,8 +43,27 @@ void test_hashmap(void) {
     hashmap_free(symbol_table);
 }
 
+void test_get_register() {
+    uint8_t sf = 0;
+    assert(get_register("x0", &sf) == 0);
+    assert(sf == SF_64);
+    assert(get_register("x1", &sf) == 1);
+    assert(sf == SF_64);
+    assert(get_register("w2", &sf) == 2);
+    assert(sf == SF_32);
+    assert(get_register("w3", &sf) == 3);
+    assert(sf == SF_32);
+    assert(get_register("w30", &sf) == 30);
+    assert(sf == SF_32);
+    assert(get_register("xzr", &sf) == ZR_REG);
+    assert(sf == SF_64);
+    assert(get_register("wzr", &sf) == ZR_REG);
+    assert(sf == SF_32);
+}
+
 int main(void) {
     test_hashmap();
+    test_get_register();
     printf("All tests passed!\n");
     return EXIT_SUCCESS;
 }
