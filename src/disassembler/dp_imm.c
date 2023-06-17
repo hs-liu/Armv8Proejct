38c472cf2ca58bf5b8bb7364ff54fc60d6a81130
Scott Loo <scottlv95@gmail.com>
2023-06-17 12:58:08 +0100

    dp_imm done

--- src/disassembler/dp_imm.c
+++ src/disassembler/dp_imm.c
@@ -3,64 +3,38 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "disassemble.h"
 #include "dp_imm.h"
 
-void add_imm(FILE* fp, uint8_t rd, uint8_t rn, uint64_t imm, bool sh, uint8_t sf) {
-    // 32 bit: "add w{dest} w{src1} #imm" into a output file
-    // 64 bit: "add x{dest} x{src1} #imm" into a output file
+
+void handle_imm_arithmetic(FILE* fp, char* op_string, uint8_t dest, uint8_t src1, uint64_t imm, bool sh, uint8_t sf) {
     assert(sf == SF_32 || sf == SF_64);
     if (sf == SF_32) {
-        fprintf(fp, "add w%d, w%d, 0x%llx, lsl #%d\n", rd, rn, imm, sh ? 12 : 0);
+        fprintf(fp, "%s w%d, w%d, 0x%llx, lsl #%d\n", op_string, dest, src1, imm, sh ? 12 : 0);
     } else {
-        fprintf(fp, "add x%d, x%d, 0x%llx, lsl #%d\n", rd, rn, imm, sh ? 12 : 0);
+        fprintf(fp, "%s x%d, x%d, 0x%llx, lsl #%d\n", op_string, dest, src1, imm, sh ? 12 : 0);
     }
 }
 
-
-
-void adds_imm(FILE* fp, uint8_t dest, uint8_t src1, uint64_t imm, bool sh, uint8_t sf) {
+void handle_mov(FILE* fp, char* op_string, uint8_t rd, uint64_t imm, char *shift_string, uint8_t hw,uint8_t sf) {
     assert(sf == SF_32 || sf == SF_64);
-    if (sf == SF_32) {
-        fprintf(fp, "adds w%d, w%d, %llx, lsl #%d\n", dest, src1, imm, sh ? 12 : 0);
-    } else {
-        fprintf(fp, "adds x%d, x%d, %llx, lsl #%d\n", dest, src1, imm, sh ? 12 : 0);
+    if (strcmp(op_string,"movz") == 0 && hw == 0) {
+        if (sf == SF_32) {
+            fprintf(fp, "mov w%d, #0x%llx\n", rd, imm);
+            return;
+        } else {
+            fprintf(fp, "mov x%d, #0x%llx\n", rd, imm);
+            return;
+        }
     }
-}
-
-
-void sub_imm(FILE* fp, uint8_t dest, uint8_t src1, uint64_t imm, bool sh, uint8_t sf) {
-    // 32 bit: "sub w{dest} w{src1} #imm" into a output file
-    // 64 bit: "sub x{dest} x{src1} #imm" into a output file
-    assert(sf == SF_32 || sf == SF_64);
     if (sf == SF_32) {
-        fprintf(fp, "sub w%d, w%d, %llx, lsl #%d\n", dest, src1, imm, sh ? 12 : 0);
+        fprintf(fp, "%s w%d, 0x%llx, %s #%d\n", op_string, rd, imm, shift_string, 16 * hw);
     } else {
-        fprintf(fp, "sub x%d, x%d, %llx, lsl #%d\n", dest, src1, imm, sh ? 12 : 0);
+        fprintf(fp, "%s x%d, 0x%llx, %s #%d\n", op_string, rd, imm, shift_string, 16 * hw);
     }
-}
 
-
-void subs_imm(FILE* fp, uint8_t dest, uint8_t src1, uint64_t imm, bool sh, uint8_t sf) {
-    assert (sf == SF_32 || sf == SF_64);
-    if (sf == SF_32) {
-        fprintf(fp, "subs w%d, w%d, %llx, lsl #%d\n", dest, src1, imm, sh ? 12 : 0);
-    } else {
-        fprintf(fp, "subs x%d, x%d, %llx, lsl #%d\n", dest, src1, imm, sh ? 12 : 0);
-    }
-}
-
-void movn_imm(FILE* fp, uint8_t dest, uint64_t imm, uint8_t sf, uint8_t hw) {
-    assert(sf == SF_32 || sf == SF_64);
-}
-
-void movz_imm(FILE* fp, uint8_t dest, uint64_t imm, uint8_t sf) {
-    assert(sf == SF_32 || sf == SF_64);
-}
-
-void movk_imm(FILE* fp, uint8_t dest, uint8_t hw, uint64_t imm, uint8_t sf) {
-    assert(sf == SF_32 || sf == SF_64);
 }
 
 void disassemble_dpimm_instruction(FILE* fp, uint32_t instruction) {
@@ -76,37 +50,40 @@ void disassemble_dpimm_instruction(FILE* fp, uint32_t instruction) {
         uint8_t sh = SELECT_BITS(instruction, IMM_SH_OFFSET, IMM_SH_SIZE);
         uint32_t imm12 = SELECT_BITS(instruction, IMM_IMM12_OFFSET, IMM_IMM12_SIZE);
         uint8_t rn = SELECT_BITS(instruction, IMM_RN_OFFSET, IMM_RN_SIZE);
+        char op_str[5];
         switch (opc) {
             case ADD_OPC:
-                add_imm(fp, rd, rn, imm12, sh, sf);
-                break;
+                strcpy(op_str, "add");
             case ADDS_OPC:
-                adds_imm(fp, rd, rn, imm12,sh, sf);
+            strcpy(op_str, "adds");
                 break;
             case SUB_OPC:
-                sub_imm(fp, rd, rn, imm12, sh,sf);
+            strcpy(op_str, "sub");
                 break;
             case SUBS_OPC:
-                subs_imm(fp, rd, rn, imm12, sh, sf);
+            strcpy(op_str, "subs");
                 break;
         }
+        handle_imm_arithmetic(fp, op_str, rd, rn, imm12, sh, sf);
     }
 
     if (opi == IMM_WIDE_MOVE_OPI) {
         uint8_t hw = SELECT_BITS(instruction, IMM_HW_OFFSET, IMM_HW_SIZE);
         uint64_t imm16 = SELECT_BITS(instruction, IMM_IMM16_OFFSET, IMM_IMM16_SIZE);
-        uint64_t op = imm16 << (16 * hw);
+        // uint64_t op = imm16 << (16 * hw);
 
+        char op_str[5];
         switch (opc) {
             case MOVN_OPC:
-                movn_imm(fp, rd, op, sf, hw);
+            strcpy(op_str, "movn");
                 break;
             case MOVZ_OPC:
-                movz_imm(fp, rd, op, sf);
+            strcpy(op_str, "movz");
                 break;
             case MOVK_OPC:
-                movk_imm(fp, rd, hw, imm16, sf);
+            strcpy(op_str, "movk");
                 break;
         }
+        handle_mov(fp, op_str, rd, imm16, "lsl", hw, sf);
     }
 }
