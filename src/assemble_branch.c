ee6609cc95aa214e1ce71fc07d11398590f0a4a0
Jerome Leow <jhl21@ic.ac.uk>
2023-06-17 13:16:49 +0100

    Fixed bug in conditional and register branch
    Coauthored by Scott

--- src/assemble_branch.c
+++ src/assemble_branch.c
@@ -37,6 +37,53 @@ void assemble_unconditional_branch(char *target_str, int target_str_len, char *l
     memcpy(state->memory + state->address, &instruction, WORD_SIZE_BYTES);
 }
 
+void assemble_reg_branch(char *target_str, assembler_state_t *state) {
+    uint64_t instruction = 0;
+    SET_BITS(instruction, ENCODING_OFFSET, ENCODING_SIZE, REGISTER_BRANCH_VALUE);
+    uint8_t sf = SF_64;
+    uint8_t xn = get_register(target_str, &sf);
+    if (sf != SF_64) {
+        fprintf(stderr, "Jump target must be 64-bit register!\n");
+        fprintf(stderr, "Exiting!\n");
+        exit(EXIT_FAILURE);
+    }
+    SET_BITS(instruction, BRANCH_REG_OFFSET, BRANCH_REG_SIZE, xn);
+    memcpy(state->memory + state->address, &instruction, WORD_SIZE_BYTES);
+}
+
+void assemble_cond_branch(char *opcode, assembler_state_t *state) {
+    uint64_t instruction = 0;
+    SET_BITS(instruction, ENCODING_OFFSET, ENCODING_SIZE, CON_BRANCH_VALUE);
+    char *condition_str = opcode + 2;
+    if (strcmp(condition_str, "eq") == 0) {
+        SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_EQ);
+    }
+    else if (strcmp(condition_str, "ne") == 0) {
+        SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_NE);
+    }
+    else if (strcmp(condition_str, "ge") == 0) {
+        SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_GE);
+    }
+    else if (strcmp(condition_str, "lt") == 0) {
+        SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_LT);
+    }
+    else if (strcmp(condition_str, "gt") == 0) {
+        SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_GT);
+    }
+    else if (strcmp(condition_str, "le") == 0) {
+        SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_LE);
+    }
+    else if (strcmp(condition_str, "al") == 0) {
+        SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_AL);
+    }
+    else {
+        fprintf(stderr, "Invalid branch condition: %s\n", condition_str);
+        fprintf(stderr, "Exiting!\n");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(state->memory + state->address, &instruction, WORD_SIZE_BYTES);
+}
+
 void assemble_branch_instruction(char *opcode, char *line, assembler_state_t *state) {
     char *target_str = strtok(NULL, " ");
     int target_str_len;
@@ -51,48 +98,10 @@ void assemble_branch_instruction(char *opcode, char *line, assembler_state_t *st
         assemble_unconditional_branch(target_str, target_str_len, line, state);
     }
     else if (strcmp(opcode, "br") == 0) {
-        uint64_t instruction = 0;
-        SET_BITS(instruction, ENCODING_OFFSET, ENCODING_SIZE, REGISTER_BRANCH_VALUE);
-        uint8_t sf = SF_64;
-        uint8_t xn = get_register(target_str, &sf);
-        if (sf != SF_64) {
-            fprintf(stderr, "Invalid line: %s\n", line);
-            fprintf(stderr, "Jump target must be 64-bit register!\n");
-            fprintf(stderr, "Exiting!\n");
-            exit(EXIT_FAILURE);
-        }
-        SET_BITS(instruction, BRANCH_REG_OFFSET, BRANCH_REG_SIZE, xn);
+        assemble_reg_branch(target_str, state);
     }
     else if (strncmp(opcode, "b.", 2) == 0) {
-        uint64_t instruction = 0;
-        SET_BITS(instruction, ENCODING_OFFSET, ENCODING_SIZE, CON_BRANCH_VALUE);
-        char *condition_str = opcode + 2;
-        if (strcmp(condition_str, "eq") == 0) {
-            SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_EQ);
-        }
-        else if (strcmp(condition_str, "ne") == 0) {
-            SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_NE);
-        }
-        else if (strcmp(condition_str, "ge") == 0) {
-            SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_GE);
-        }
-        else if (strcmp(condition_str, "lt") == 0) {
-            SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_LT);
-        }
-        else if (strcmp(condition_str, "gt") == 0) {
-            SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_GT);
-        }
-        else if (strcmp(condition_str, "le") == 0) {
-            SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_LE);
-        }
-        else if (strcmp(condition_str, "al") == 0) {
-            SET_BITS(instruction, BRANCH_COND_OFFSET, BRANCH_COND_SIZE, COND_AL);
-        }
-        else {
-            fprintf(stderr, "Invalid branch condition: %s\n", condition_str);
-            fprintf(stderr, "Exiting!\n");
-            exit(EXIT_FAILURE);
-        }
+        assemble_cond_branch(opcode, state);
     }
 
     state->address += WORD_SIZE_BYTES;
