f57cf295fc10f1ebd998610ee114543a9eea3e9a
Yukie <yz8922@ic.ac.uk>
2023-06-18 19:52:53 +0100

    we worked together on arm64 program for rpi from scratch; defined memory values, loops, and 2s wait;

--- rpi/led.s
+++ rpi/led.s
@@ -1,78 +1,49 @@
-.section .data
-mailbox_base:   .equ 0x3F000000
-mailbox_offset: .equ 0xB880
-read_offset:    .equ 0x00
-write_offset:   .equ 0x20
-status_offset:  .equ 0x18
-led_channel:    .equ 8
-led_on_msg:     .equ 0x00010001
-led_off_msg:    .equ 0x00010000
-delay_count:    .equ 20000000
+ldr w0, #0x3f00b8A0 // address of mailbox write reg
+ldr w1, #0x3F00B8B8 // addr of mailbox status reg
 
-.section .text
-.global _start
+ldr w2, #0x00000020 // buffer start
+orr w2, wzr, wzr, lsl #0x04
+movz w3, #0x00038041 // temp value holder reg
+orr w2, wzr, w3, lsl #0x08
+movz w3, #0x00000008
+orr w2, wzr, w3, lsl #0x0c
+orr w2, wzr, wzr, lsl #0x010
+movz w3, #0x00000082
+orr w2, wzr, w3, lsl #0x014
+movz w3, #0x00000001
+orr w2, wzr, w3, lsl #0x18 //led_status: on
+orr w2, wzr, wzr, lsl #0x1c
 
-_start:
-    // Open /dev/mem
-    mov x0, 0
-    mov x1, 0x01C00000
-    mov x2, 0x10
-    mov x7, 8
-    svc 0
+ldr w4, #0x8 //message: channel
+ldr x3, [x4]
+add w3, w3, #0x80000, lsl #0
+orr w4, wzr, w3, lsl #4
 
-    // Map peripheral base to virtual memory
-    lsl x1, x0, #12
-    add x0, x1, mailbox_offset
-    mov x1, 0x1000
-    lsl x2, x0, #1
-    orr x2, x2, #0x0000001C
-    lsl x3, x2, #12
-    mov x7, 0
-    mov x8, 0x3
-    mov x0, 0
-    mov x2, 0x00000000
-    mov x3, 0x00000000
-    mov x4, 0x00000000
-    mov x5, 0x00000000
-    mov x6, 0x00000000
-    mov x7, 0x00000000
-    svc 0
+check_write_empty:
+    ldr w5, #0x1000
+    orr w5, wzr, w1, lsr #31 // get F flag
+    cmp w5, wzr
+    b.ne check_write_empty // can write msg if zero
 
-    // Main loop
-loop:
-    // Turn on LED
-    lsl x1, x0, #2
-    ldr x2, =led_on_msg
-    orr x2, x2, x1, lsl #4
-    str x2, [x0, write_offset]
+on_loop:
+    ldr x0, [x4] // load msg into write_reg
+    movz x3, #0x0002, lsl #16
 
-    // Delay
-    mov x1, delay_count
-delay:
-    subs x1, x1, #1
-    bne delay
+wait1:
+    sub x3, x3, #1
+    cmp x3, #0
+    b.ne wait1
+    
+off_loop:
+    orr w2, wzr, wzr, lsl #0x18 // set led_off; TODO: reset buffer values
+    movz x3, #0x0002, lsl #16
 
-    // Turn off LED
-    lsl x1, x0, #2
-    ldr x2, =led_off_msg
-    orr x2, x2, x1, lsl #4
-    str x2, [x0, write_offset]
+wait2:
+    sub x3, x3, #1
+    cmp x3, #0
+    b.ne wait2
+    b on_loop
 
-    // Delay
-    mov x1, delay_count
-delay2:
-    subs x1, x1, #1
-    bne delay2
-
-    b loop
-
-    // Exit program
-exit:
-    mov x8, 0x18
-    mov x0, 0
-    mov x1, 0
-    mov x2, 0
-    mov x3, 0
-    mov x4, 0
-    mov x7, 0x10
-    svc 0
+//TODO: reset buffer values;
+//read register
+//check_queue_size
