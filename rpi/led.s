f9ae634d6e2bfec8ae4855a02f2017882f4d03bc
Hongshuo Liu <hl2622@aube.doc.ic.ac.uk>
2023-06-20 11:50:37 +0100

    refactored check_status label, combine on_loop and off_loop

--- rpi/led.s
+++ rpi/led.s
@@ -19,31 +19,35 @@ ldr x3, [x4]
 add w3, w3, #0x80000, lsl #0
 orr w4, wzr, w3, lsl #4
 
-check_write_empty:
-    ldr w5, #0x1000
+check_write_empty: //every send msg need to check F empty
+    ldr w5, wzr
     orr w5, wzr, w1, lsr #31 // get F flag
     cmp w5, wzr
     b.ne check_write_empty // can write msg if zero
 
-on_loop:
-    ldr x0, [x4] // load msg into write_reg
+check_status:
+    orr w5, wzr, w2, lsr #0x18//check_status
+    cmp w5, wzr
+    b.eq  set_on//if status = off, go set on
+    orr w2, w2, w2, lsl #0x18//if status = on, set off <?>
+
+send_msg_loop:
+    ldr x0, [x4] // send msg into write_reg
     movz x3, #0x0002, lsl #16
 
-wait1:
+wait1: //first wait
     sub x3, x3, #1
     cmp x3, #0
     b.ne wait1
+    b check_write_empty
     
-off_loop:
-    orr w2, wzr, wzr, lsl #0x18 // set led_off; TODO: reset buffer values
-    movz x3, #0x0002, lsl #16
-
-wait2:
-    sub x3, x3, #1
-    cmp x3, #0
-    b.ne wait2
-    b on_loop
+set_on:
+    movz x3, #0x00000001
+    orr w2, w2, x3, lsl #0x18
+    b send_msg_loop
 
 //TODO: reset buffer values;
+reset_buffer:
+
 //read register
-//check_queue_size
+//check_queue_sizegit
